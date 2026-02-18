5222851756e89f9274641c4276f909975ed71d69
SHAHZAINDAVID005 <78403916+SHAHZAINDAVID005@users.noreply.github.com>
2026-02-18 10:51:43 +0500

    Add files via upload

---
+++ env.js
@@ -0,0 +1,19 @@
+// env.js
+// This file contains all your sensitive information.
+
+export const USERNAME = "alijoiya658@gmail.com";
+export const PASSWORD = "Zainjoiya21";
+
+export const BOT_TOKEN = "7959345166:AAEvGLce7goj9pSWL0PQhy8RVeG6y_fac98";
+export const CHAT_ID = "-1003243421314";
+
+export const REFRESH_INTERVAL_MINUTES = 30;
+export const AUDIO_TRANSCRIPTION_RETRIES = 1;
+
+
+export const MAIN_CHANNEL_NAME = "Main Channel";
+export const MAIN_CHANNEL_URL = "CHANNEL_URL";
+
+export const ADMIN_NAME = "Contact Admin";
+export const ADMIN_URL = "ADMIN_URL";
+
